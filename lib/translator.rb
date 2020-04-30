671188f6072b8ba92d2cdbc73bd3b2faa8609b5a
Youngmin Park <codepark2019@gmail.com>
2020-04-30 23:29:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,25 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(path)
+  library = YAML.load_file(path)
+  new_hash = {}
+  get_meaning = {}
+  get_emoticon = {}
+  library.each do |key, value|
+    get_meaning[value[1]] = key
+    get_emoticon[value[0]] = value[1]
+  end
+  new_hash["get_meaning"] = get_meaning
+  new_hash["get_emoticon"] = get_emoticon
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  hash = load_library(path)
+  
+
 end
 
 def get_english_meaning
